c17b36fb92a7eb483acd9f781dd16be2390cf16a
qg46 <78160128+qg46@users.noreply.github.com>
2025-12-20 15:37:52 +0800

    新增返回配置项siteIcon

--- node-functions/api/[[default]].js
+++ node-functions/api/[[default]].js
@@ -92,7 +92,8 @@ const SECURITY_METRICS = [
 
 app.get('/config', (req, res) => {
     res.json({
-        siteName: process.env.SITE_NAME || 'AcoFork 的 EdgeOne 监控大屏'
+        siteName: process.env.SITE_NAME || 'AcoFork 的 EdgeOne 监控大屏',
+        siteIcon: process.env.SITE_ICON || 'https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=0'
     });
 });
 
